e46a306dcc9d82d42c9ca4c051de172ac9f19458
noobydev546 <donutaaaa19@gmail.com>
2025-09-04 15:59:47 +0700

    Refactor counter widget: streamline update method and enhance UI structure

--- lib/week01/counter.dart
+++ lib/week01/counter.dart
@@ -11,21 +11,34 @@ class _CounterState extends State<Counter> {
   //state variable
   int count = 0;
 
+  //methods
+  void update() {
+    setState(() {
+      count++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Counter app')),
-      body: Text('Count = $count'),
+      appBar: AppBar(
+        title: Text(
+          'Counter app',
+          style: TextStyle(fontSize: 32, color: Colors.white),
+        ),
+        backgroundColor: Color.fromARGB(255, 0, 0, 0),
+      ),
+      body: Center(
+        child: Text(
+          'Count = $count',
+          style: TextStyle(fontSize: 26, color: Color.fromARGB(255, 0, 0, 0)),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // debugPrint(message);
-          setState(() {
-            count++;
-          });
-        },
-        child: Text('Click'),
+        onPressed: update,
+        backgroundColor: Color.fromARGB(255, 0, 0, 0),
+        child: Text('Click', style: TextStyle(color: Colors.white)),
       ),
     );
-    ;
   }
 }
