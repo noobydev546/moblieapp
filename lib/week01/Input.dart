cf16a4fac8b2f0dc37e9d5c9ded1838cf3de2b4a
noobydev546 <donutaaaa19@gmail.com>
2025-09-04 20:05:21 +0700

    Complete Input feature

--- lib/week01/Input.dart
+++ lib/week01/Input.dart
@@ -8,53 +8,68 @@ class InputDemo extends StatefulWidget {
 }
 
 class _InputDemoState extends State<InputDemo> {
-  String message = '';
+  final List<Map<String, String>> users = [
+    {"username": "admin", "password": "1111"},
+    {"username": "test", "password": "2222"},
+  ];
+
+  String username = '';
+  String password = '';
+  String err_msg = '';
+
   TextEditingController tcName = TextEditingController();
   TextEditingController tcPassword = TextEditingController();
 
   void updatemessage() {
     setState(() {
-      message = tcName.text;
+      username = tcName.text;
+      password = tcPassword.text;
+
+      err_msg = "Wrong username or password";
+
+      for (var user in users) {
+        if (user['username'] == username && user['password'] == password) {
+          err_msg = "Welcome $username";
+          break;
+        }
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Input Demo')),
+      appBar: AppBar(
+        title: Text(
+          'Login',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.blue,
+      ),
       body: Column(
         children: [
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: TextField(
               controller: tcName,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Input your name',
-                suffixIcon: IconButton(
-                  onPressed: tcName.clear,
-                  icon: Icon(Icons.clear),
-                ),
-              ),
+              decoration: InputDecoration(hintText: 'Username'),
             ),
           ),
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: TextField(
-              controller: tcName,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Input your password',
-                suffixIcon: IconButton(
-                  onPressed: tcPassword.clear,
-                  icon: Icon(Icons.clear),
-                ),
-              ),
+              controller: tcPassword,
+              obscureText: true,
+              decoration: InputDecoration(hintText: 'Password'),
             ),
           ),
-          ElevatedButton(onPressed: updatemessage, child: Text('OK')),
+          ElevatedButton(
+            onPressed: updatemessage,
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+            child: Text('Login', style: TextStyle(color: Colors.white)),
+          ),
           SizedBox(height: 16),
-          Text(message),
+          Text(err_msg, style: TextStyle(color: Colors.red)),
         ],
       ),
     );
