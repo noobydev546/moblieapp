19a7be7f899c280b9fe131cb74dad09d0375cdf8
noobydev546 <donutaaaa19@gmail.com>
2025-09-11 15:50:10 +0700

    complete design 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:moblieapp/week05/multi_child_demo.dart';
+import 'package:moblieapp/week05/design1.dart';
 
 void main() {
-  runApp(
-    MaterialApp(home: MultiChildDemo(), debugShowCheckedModeBanner: false),
-  );
+  runApp(MaterialApp(home: Design1(), debugShowCheckedModeBanner: false));
 }
