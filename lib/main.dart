7007efde953ed3fa4a0887877f24d158e7c1d5e3
noobydev546 <donutaaaa19@gmail.com>
2025-09-13 18:45:57 +0700

    Complete Assignment 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:moblieapp/week05/random_demo.dart';
+import 'package:moblieapp/week05/assignment2.dart';
 
 void main() {
-  runApp(MaterialApp(home: RandomDemo(), debugShowCheckedModeBanner: false));
+  runApp(MaterialApp(home: Assignment2(), debugShowCheckedModeBanner: false));
 }
---
+++ lib/week05/assignment2.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class Assignment2 extends StatefulWidget {
+  const Assignment2({super.key});
+
+  @override
+  State<Assignment2> createState() => _Assignment2State();
+}
+
+class _Assignment2State extends State<Assignment2> {
+  TextEditingController tcNumber1 = TextEditingController();
+  TextEditingController tcNumber2 = TextEditingController();
+  String result = '';
+
+  void calculate() {
+    int? num1 = int.tryParse(tcNumber1.text);
+    int? num2 = int.tryParse(tcNumber2.text);
+    int sum = 0;
+
+    if (num1 == null || num2 == null) {
+      setState(() {
+        result = 'Incorrect input';
+      });
+      return;
+    }
+    sum = num1 + num2;
+
+    setState(() {
+      result = 'Result = $sum';
+    });
+  }
+
+  void clear() {
+    tcNumber1.clear();
+    tcNumber2.clear();
+    setState(() {
+      result = '';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: TextField(
+                      controller: tcNumber1,
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        hintText: 'First number',
+                      ),
+                    ),
+                  ),
+                ),
+                const Text('+', style: TextStyle(fontWeight: FontWeight.bold)),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: TextField(
+                      controller: tcNumber2,
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        hintText: 'Second number',
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FilledButton(
+                  style: FilledButton.styleFrom(backgroundColor: Colors.blue),
+                  onPressed: calculate,
+                  child: const Text('Calculate'),
+                ),
+                const SizedBox(width: 8),
+                FilledButton(
+                  style: FilledButton.styleFrom(backgroundColor: Colors.red),
+                  onPressed: clear,
+                  child: const Text('Clear'),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Text(
+              result,
+              style: const TextStyle(fontSize: 16, color: Colors.red),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
