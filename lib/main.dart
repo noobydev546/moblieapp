653a3ab3e22102a8f1372258daaa4c197b7ebcab
noobydev546 <donutaaaa19@gmail.com>
2025-09-13 18:12:33 +0700

    Complete Random demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:moblieapp/week05/design2.dart';
+import 'package:moblieapp/week05/random_demo.dart';
 
 void main() {
-  runApp(MaterialApp(home: Design2(), debugShowCheckedModeBanner: false));
+  runApp(MaterialApp(home: RandomDemo(), debugShowCheckedModeBanner: false));
 }
