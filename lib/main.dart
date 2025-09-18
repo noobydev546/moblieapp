9167be4a4f8212918643d1d2da961884142e15b5
noobydev546 <donutaaaa19@gmail.com>
2025-09-18 16:21:53 +0700

    Add login dynamic

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:moblieapp/week06/baking_app.dart';
+import 'package:moblieapp/week06/login_dynamic.dart';
 
 void main() {
-  runApp(MaterialApp(home: BakingApp(), debugShowCheckedModeBanner: false));
+  runApp(MaterialApp(home: LoginDynamic(), debugShowCheckedModeBanner: false));
 }
