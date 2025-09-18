b8a977b0e2b0100d5cbf3cecd958e052ec70d20b
noobydev546 <donutaaaa19@gmail.com>
2025-09-18 15:06:34 +0700

    Add images demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:moblieapp/week06/button_demo.dart';
+import 'package:moblieapp/week06/images_demo.dart';
 
 void main() {
-  runApp(MaterialApp(home: ButtonDemo(), debugShowCheckedModeBanner: false));
+  runApp(MaterialApp(home: ImagesDemo(), debugShowCheckedModeBanner: false));
 }
